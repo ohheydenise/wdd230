6a7b31adea2ae08c980f63dc18ace1d0fd07f5b3
ohheydenise <wil19015@byui.edu>
2023-01-25 08:57:17 -0700

    Started building the hamburger menu

--- lesson3/scripts/script.js
+++ lesson3/scripts/script.js
@@ -2,3 +2,12 @@ const today = new Date();
 
 document.querySelector('#currentyear').textContent = today.getFullYear();
 document.querySelector('#lastmodified').textContent = document.lastModified;
+
+// Toggle Hamburger Menu
+function toggleMenu() {
+    document.querySelector('#navbar').classList.toggle('menu-active')
+    document.querySelector('#menu-close').classList.toggle('menu-active')
+    document.querySelector('#menu-open').classList.toggle('menu-active')
+}
+
+document.querySelector('#hamburger').onclick = toggleMenu;
